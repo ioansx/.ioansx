dc4938da66d9587033fad555a0c849c320539363
Ioan Stănilă <ioanstanilais@gmail.com>
2024-01-24 15:45:59 +0200

    use telescope oldfiles rather than buffers for quick jumps between files

--- nvim/init.lua
+++ nvim/init.lua
@@ -413,7 +413,7 @@ vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
 vim.keymap.set("n", "<C-h>", ":noh<CR>")
 
-vim.keymap.set("n", "<leader><space>", "<C-^>", { desc = "[ ] Toggle last buffer" })
+vim.keymap.set("n", "<leader><leader>", "<C-^>", { desc = "[ ] Toggle last buffer" })
 
 vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, { desc = "Go to previous [d]iagnostic" })
 vim.keymap.set("n", "]d", vim.diagnostic.goto_next, { desc = "Go to next [d]iagnostic" })
@@ -518,15 +518,9 @@ end
 
 vim.api.nvim_create_user_command("LiveGrepGitRoot", live_grep_git_root, {})
 
-local function telescope_buffers()
-	require("telescope.builtin").buffers({
-		sort_mru = true,
-	})
-end
-
 -- See `:help telescope.builtin`
-vim.keymap.set("n", "<leader>?", require("telescope.builtin").oldfiles, { desc = "[?] Find recently opened files" })
-vim.keymap.set("n", "<leader>b", telescope_buffers, { desc = "Find existing [b]uffers" })
+vim.keymap.set("n", "<leader>f", require("telescope.builtin").oldfiles, { desc = "[f]ind recently opened files" })
+vim.keymap.set("n", "<leader>?", require("telescope.builtin").buffers, { desc = "[?] find existing buffers" })
 vim.keymap.set("n", "<leader>/", function()
 	-- You can pass additional configuration to telescope to change theme, layout, etc.
 	require("telescope.builtin").current_buffer_fuzzy_find(require("telescope.themes").get_dropdown({
