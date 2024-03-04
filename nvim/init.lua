103143200d97dc4da8a6a2ac10dca92c4503be6c
Ioan Stănilă <ioanstanilais@gmail.com>
2024-03-04 09:37:23 +0200

    nvim: migrate to conform.nvim

--- nvim/init.lua
+++ nvim/init.lua
@@ -65,66 +65,20 @@ require("lazy").setup({
             { "williamboman/mason.nvim", config = true },
             "williamboman/mason-lspconfig.nvim",
             { "j-hui/fidget.nvim",       opts = {} },
-            -- "folke/neodev.nvim",
         },
-        config = function()
-            --  Use :FormatToggle to toggle autoformatting on or off
-            local format_is_enabled = true
-            vim.api.nvim_create_user_command("FormatToggle", function()
-                format_is_enabled = not format_is_enabled
-                print("Setting autoformatting to: " .. tostring(format_is_enabled))
-            end, {})
+    },
 
-            -- Create an augroup that is used for managing our formatting autocmds.
-            --      We need one augroup per client to make sure that multiple clients
-            --      can attach to the same buffer without interfering with each other.
-            local _augroups = {}
-            local get_augroup = function(client)
-                if not _augroups[client.id] then
-                    local group_name = "lsp-format-" .. client.name
-                    local id = vim.api.nvim_create_augroup(group_name, { clear = true })
-                    _augroups[client.id] = id
-                end
-
-                return _augroups[client.id]
-            end
-
-            vim.api.nvim_create_autocmd("LspAttach", {
-                group = vim.api.nvim_create_augroup("lsp-attach-format", { clear = true }),
-                callback = function(args)
-                    local client_id = args.data.client_id
-                    local client = vim.lsp.get_client_by_id(client_id)
-                    local bufnr = args.buf
-
-                    if not client.server_capabilities.documentFormattingProvider then
-                        return
-                    end
-
-                    -- Tsserver usually works poorly. Sorry you work with bad languages
-                    -- You can remove this line if you know what you're doing :)
-                    if client.name == "tsserver" then
-                        return
-                    end
-
-                    vim.api.nvim_create_autocmd("BufWritePre", {
-                        group = get_augroup(client),
-                        buffer = bufnr,
-                        callback = function()
-                            if not format_is_enabled then
-                                return
-                            end
-
-                            vim.lsp.buf.format({
-                                async = false,
-                                filter = function(c)
-                                    return c.id == client.id
-                                end,
-                            })
-                        end,
-                    })
-                end,
-            })
-        end,
+    {
+        'stevearc/conform.nvim',
+        opts = {
+            format_on_save = {
+                timeout_ms = 500,
+                lsp_fallback = true,
+            },
+            formatters_by_ft = {
+                lua = { 'stylua' },
+            },
+        },
     },
 
     {
