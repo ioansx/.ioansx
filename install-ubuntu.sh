42365c3a705aba4fb6de69c67ba33199ede1d190
ioansx <ioanstanilais@gmail.com>
2024-10-28 19:47:20 +0100

    update linux install script and tmux bug

--- install-ubuntu.sh
+++ install-ubuntu.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+sudo apt update
+sudo apt upgrade
 sudo apt install \
 	alacritty \
 	curl \
@@ -8,6 +10,15 @@ sudo apt install \
 	git \
 	make \
 	ripgrep \
+	tmux \
 	vim \
 	zsh \
 	-y
+
+nix-channel --add https://nixos.org/channels/nixos-unstable nixos
+nix-env -iA \
+	nixpkgs.fzf \
+	nixpkgs.go \
+	nixpkgs.lazygit \
+	nixpkgs.neovim \
+	nixpkgs.nodejs_20
