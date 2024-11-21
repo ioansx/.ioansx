ddd3090b3a9f866bb7c6ad8eb88d9bafa02400d9
ioansx <ioanstanilais@gmail.com>
2024-11-21 22:00:11 +0100

    wezterm 0 padding

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -7,6 +7,12 @@ config.color_scheme = "Builtin Tango Dark"
 config.use_fancy_tab_bar = false
 config.hide_tab_bar_if_only_one_tab = true
 config.max_fps = 120
+config.window_padding = {
+    left = 0,
+    top = 0,
+    right = 0,
+    bottom = 0,
+}
 
 -- Font
 config.font = wezterm.font "FiraCode Nerd Font Mono"
