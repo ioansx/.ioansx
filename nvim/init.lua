a0f6659502f860085035328f2be91193ecc2450d
Stănilă Ioan <ioanstanilais@gmail.com>
2025-02-17 11:03:01 +0100

    nvim todo-comments

--- nvim/init.lua
+++ nvim/init.lua
@@ -403,14 +403,14 @@ require("lazy").setup({
         opts = { signs = false },
         keys = {
             {
-                "<leader>sT",
-                function() Snacks.picker.todo_comments() end,
-                desc = "Todo"
-            },
-            {
                 "<leader>st",
                 function() Snacks.picker.todo_comments({ keywords = { "TODO", "FIX", "FIXME" } }) end,
                 desc = "Todo/Fix/Fixme"
+            },
+            {
+                "<leader>sT",
+                function() Snacks.picker.todo_comments() end,
+                desc = "Todo"
             },
         }
     },
