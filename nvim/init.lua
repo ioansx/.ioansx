ed0ca6364aa57d66ba8e931d3aeb930520496d0e
Stănilă Ioan <ioanstanilais@gmail.com>
2025-08-05 08:55:43 +0200

    tab is 4

--- nvim/init.lua
+++ nvim/init.lua
@@ -12,10 +12,9 @@ vim.opt.ignorecase = true
 vim.opt.smartcase = true
 vim.opt.gdefault = true
 
--- vim.opt.expandtab = true
-vim.opt.softtabstop = 8
-vim.opt.shiftwidth = 8
-vim.opt.tabstop = 8
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.tabstop = 4
 
 vim.opt.breakindent = true
 vim.opt.smartindent = true
