0a93127d475fa6cdb546e8e4f111046bff9476f4
Stănilă Ioan <ioanstanilais@gmail.com>
2025-09-28 19:50:26 +0200

    nice fish prompt

--- fish/config.fish
+++ fish/config.fish
@@ -1,24 +1,36 @@
 if status is-interactive
-    # Commands to run in interactive sessions can go here
+        # Commands to run in interactive sessions can go here
 end
 
 fish_vi_key_bindings
 # set fish_command_timer_time_format '%a %d %H:%M'
 
-set -x XDG_CONFIG_HOME "$HOME/.config"
-
-# cargo path
-set -U fish_user_paths $HOME/.cargo/bin $fish_user_paths
-
-# user binaries and scripts
-set -U fish_user_paths $HOME/.local/bin $fish_user_paths
 
 alias lg="lazygit"
+alias man='MANWIDTH=80 man'
+
+set -x XDG_CONFIG_HOME "$HOME/.config"
+set -U fish_user_paths $HOME/.cargo/bin $fish_user_paths
+set -U fish_user_paths $HOME/.local/bin $fish_user_paths
+
+if [ -f '/Users/ioan/google-cloud-sdk/path.fish.inc' ]; . '/Users/ioan/google-cloud-sdk/path.fish.inc'; end
 
 fzf --fish | source
 zoxide init fish | source
 
-if [ -f '/Users/ioan/google-cloud-sdk/path.fish.inc' ]; . '/Users/ioan/google-cloud-sdk/path.fish.inc'; end
+function humantime --argument-names ms --description "Turn milliseconds into a human-readable string"
+    set --query ms[1] || return
+
+    set --local secs (math --scale=1 $ms/1000 % 60)
+    set --local mins (math --scale=0 $ms/60000 % 60)
+    set --local hours (math --scale=0 $ms/3600000)
+
+    test $hours -gt 0 && set --local --append out $hours"h"
+    test $mins -gt 0 && set --local --append out $mins"m"
+    test $secs -gt 0 && set --local --append out $secs"s"
+
+    set --query out && echo $out || echo $ms"ms"
+end
 
 function fish_prompt --description 'Write out the prompt'
         set -l last_pipestatus $pipestatus
@@ -49,5 +61,5 @@ function fish_prompt --description 'Write out the prompt'
         set -l statusb_color (set_color $bold_flag $fish_color_status)
         set -l prompt_status (__fish_print_pipestatus "[" "]" "|" "$status_color" "$statusb_color" $last_pipestatus)
 
-        echo -n -s (set_color $color_cwd) (prompt_pwd --full-length-dirs 13) $normal " "$prompt_status $suffix " "
+        echo -n -s (humantime $CMD_DURATION) (set_color $color_cwd) " [" (date "+%Y-%m-%d %T") "] " (prompt_pwd --full-length-dirs 13) $normal " "$prompt_status $suffix " "
 end
