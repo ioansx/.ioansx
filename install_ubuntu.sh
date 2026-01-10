5859544ece50de8dff8b75fe98c264ba9baac0d8
ioansx <ioanstanilais@gmail.com>
2026-01-10 12:48:37 +0100

    update install_ubuntu script

--- install_ubuntu.sh
+++ install_ubuntu.sh
@@ -6,6 +6,8 @@ sudo apt update && apt upgrade
 sudo apt install \
 	cmake \
 	curl \
+	fish \
+	fzf \
 	gcc \
 	git \
 	htop \
@@ -16,7 +18,9 @@ sudo apt install \
 	wl-clipboard \
 	-y
 
-# Neovim dependencies
+sudo snap install ghostty --classic
+
+sudo snap install nvim --classic
 sudo apt install \
 	fd-find \
 	ripgrep \
@@ -26,5 +30,26 @@ sudo apt install \
 sudo snap install \
 	pgadmin4
 
+
+if ! [ -x "$(command -v rustup)" ]; then
+	curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+fi
+
+if ! [ -x "$(command -v pulumi)" ]; then
+	curl -fsSL https://get.pulumi.com | sh
+fi
+
 # Install docker
 # https://docs.docker.com/engine/install/ubuntu/
+if ! [ -x "$(command -v docker)" ]; then
+	# sudo apt remove -y docker \
+	# 	docker-client \
+	# 	docker-client-latest \
+	# 	docker-common \
+	# 	docker-latest \
+	# 	docker-latest-logrotate \
+	# 	docker-logrotate \
+	# 	docker-selinux \
+	# 	docker-engine-selinux \
+	# 	docker-engine
+fi
